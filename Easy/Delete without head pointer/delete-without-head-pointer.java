13c9c2929a8c567fa86590ba6fc7a4e0cc5ffac5
Akshay Singh <singhakshay8081@gmail.com>
2024-03-16 01:48:00 +0530

    Updated Solution - GfG to GitHub

--- Easy/Delete without head pointer/delete-without-head-pointer.java
+++ Easy/Delete without head pointer/delete-without-head-pointer.java
@@ -111,9 +111,8 @@ class Solution
 {
     void deleteNode(Node del_node)
     {
-         // Your code here
-           del_node.data = del_node.next.data;
-        del_node.next = del_node.next.next;
+         del_node.data=del_node.next.data;
+      del_node.next=del_node.next.next;
     }
 }
 
