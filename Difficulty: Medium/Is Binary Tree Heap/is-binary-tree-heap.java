3d23efbc6c1f64f76a8a5e3dd3d1fd56f70927d1
Akshay Singh <singhakshay8081@gmail.com>
2025-04-26 01:43:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.java
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution ob = new Solution();
+            if (ob.isHeap(root))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User Function template for JAVA
+
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution {
+    boolean isHeap(Node tree) {
+        // code here
+         if(isComplete(tree) && solve(tree)!=-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    int solve(Node root){
+        if(root==null){
+            return 0;
+        }
+        int left=solve(root.left);
+        int right=solve(root.right);
+        if(left!=-1 && right!=-1 && left<=root.data && right<root.data){
+            return root.data;
+        }else{
+            return -1;
+        }
+    }
+    
+    boolean isComplete(Node root){
+        if(root==null){
+            return true;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        boolean end=false;
+        while(!q.isEmpty()){
+            Node node=q.remove();
+            if(node==null){
+                end=true;
+            }else{
+                if(end){
+                    return false;
+                }else{
+                    q.add(node.left);
+                    q.add(node.right);
+                }
+            }
+        }
+        
+        return true;
+    }
+}
