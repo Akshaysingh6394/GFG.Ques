f1261eadf9a53a74f2fde400173d025df611fb56
Akshay Singh <singhakshay8081@gmail.com>
2025-05-06 13:49:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            ArrayList<Integer> result = g.leftView(root);
+            if (result.size() == 0) {
+                System.out.print("[]");
+            }
+            for (int value : result) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Solution {
+    // Function to return list containing elements of left view of binary tree.
+    void f(Node node, ArrayList<Integer> arr, int lvl){
+        if(node==null)return;
+        if(arr.size()<lvl)arr.add(node.data);
+        
+        f(node.left, arr, lvl+1);
+        f(node.right, arr, lvl+1);
+    }
+    
+    ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        f(root, ans,1);
+        return ans;
+    }
+}
