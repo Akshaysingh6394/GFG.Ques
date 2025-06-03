4b4ab21e0954ffa4d0c6a8075516e96399d3de23
Akshay Singh <singhakshay8081@gmail.com>
2025-06-03 20:04:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int countSubstr(string& s, int k) {
+        // code here.
+           int count = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        vector<int> freq(26, 0);
+        int distinct = 0;
+        for (int j = i; j < s.size(); j++)
+        {
+            if (freq[s[j] - 'a'] == 0)
+            {
+                distinct++;
+            }
+            freq[s[j] - 'a']++;
+            if (distinct == k)
+            {
+                count++;
+            }
+            if (distinct > k)
+                break;
+        }
+    }
+    return count;
+    }
+};
